34f461518067b9c66afe25a944fde845416b9d74
apavlin <apavlin@yandex.ru>
2016-02-07 10:02:39 +0700

    asana hover(js), icons

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 > Instructions
 
-1. git clone https://github.com/apavlin/workflow1 nameOfProject
+To install this project:
+
+1. git clone https://github.com/apavlin/yogi42 nameOfProject
 2. cd nameOfProject
 3. bower i; npm i; gulp
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
-console.log("in main.js")
+
+(function($){
+
+
+
+$('.oneA').hover(function(){
+    $('.asana-img',this).css({'opacity':'0.05'});
+    $('.asana-header, .asana-text',this).css({'opacity':'1'});
+  }, (function(){
+    $('.asana-img',this).css({'opacity':'0.25'});
+    $('.asana-header, .asana-text',this).css({'opacity':'0'});
+  }));
+
+
+
+  }(jQuery));
